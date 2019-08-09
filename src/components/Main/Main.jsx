bf765f5f1daca7350374d92af7183856bbec5556
Stefano Le Pera <stefano.lepera@gmail.com>
2019-08-09 21:45:12 +0100

    refactored Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Main = () => <p>React Hooks Playground</p>;
+const MainWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-item: center;
+    justify-content: center;
+`;
+
+const Main = () => {
+    return (
+        <MainWrapper>
+            <h2>React Hooks Playground</h2>
+            <p>
+                Please checkout the first branch <b>EX1_useState_basic</b> and
+                solve the problem
+            </p>
+        </MainWrapper>
+    );
+};
 
 export default Main;
