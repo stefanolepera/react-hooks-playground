3c73c175f4df1e9bdb3855bd730d85d126a695f2
Stefano Le Pera <stefano.lepera@gmail.com>
2019-08-10 22:59:29 +0100

    small refactor Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -23,13 +23,11 @@ class Main extends Component {
                 <h2>EX2_useState_intermediate</h2>
                 <p>my credentials are:</p>
                 <List>
-                    {Object.keys(this.state).map(credential => {
-                        return (
-                            <ListItem>{`${credential}: ${
-                                this.state[credential]
-                            }`}</ListItem>
-                        );
-                    })}
+                    {Object.keys(this.state).map(credential => (
+                        <ListItem>
+                            {`${credential}: ${this.state[credential]}`}
+                        </ListItem>
+                    ))}
                 </List>
                 <Button onClick={this.changeName}>CHANGE NAME</Button>
             </MainWrapper>
