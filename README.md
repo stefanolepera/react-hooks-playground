59a1517d6bc804d70b7e2f6a9b9389d02d2b3b64
Stefano Le Pera <stefano.lepera@gmail.com>
2019-08-11 14:07:09 +0100

    EX5 solution implemented

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-## React Hooks Playground - EX5_useEffect_intermediate
+## React Hooks Playground - EX5_useEffect_intermediate_solution
 
-To solve this exercise, simply update the `Main.jsx` component using the `useEffect` to listen to the window resize.
+In order to solve this exercise, we need to use multiple `useEffect`, which is always a good idea to separate concerns (Single Responsibility principle)<br>
+In the first `useEffect` we add the listener for the `resize` event, while the second just update the document title.<br>
+Couple of things to notice: we pass an empty array `[]` to the first `useEffect` so that runs only once, after the first render, but to the second one, we pass `[width]` so that hook will trigger every time the `width` value changes.<br>
 
-**Note: we use `PureComponent` here to optimize performance and avoid unnecessary re-renders**
+```javascript
+useEffect(() => {
+    // this runs only once, after the first render
+}, []);
 
-If you find yourself stuck or just want to double check your solution, please check out the [EX5_useEffect_intermediate_solution](https://github.com/stefanolepera/react-hooks-playground/tree/EX5_useEffect_intermediate_solution) branch.
+useEffect(() => {
+    // this runs every time the width value changes
+}, [width]);
+```
+
+Both hooks return a clean up function, which runs before the component is removed from the UI to prevent memory leaks. That does the same job as `componentWillUnmount`<br>
+
+```javascript
+useEffect(() => {
+    // we remove the listener when the component is removed from the UI
+    return () => window.removeEventListener('resize', handleResize);
+}, []);
+
+useEffect(() => {
+    // we reset the document title when the component is removed from the UI
+    return () => (document.title = '');
+}, [width]);
+```
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,45 +1,27 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MainWrapper } from './Main.style';
 
-class Main extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            width: window.innerWidth,
-            isTooSmall: false
-        };
-    }
+const Main = () => {
+    const [width, setWidth] = useState(window.innerWidth);
 
-    componentDidMount() {
-        window.addEventListener('resize', this.handleResize);
-    }
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
-    componentDidUpdate(_, prevState) {
-        if (prevState.width !== this.state.width) {
-            if (this.state.width < 300) {
-                this.setState({ warning: true });
-            } else {
-                this.setState({ warning: false });
-            }
-        }
-    }
+    useEffect(() => {
+        const title = width < 300 ? "It's too small!" : "It's ok";
+        document.title = title;
+        return () => (document.title = '');
+    }, [width]);
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleResize);
-    }
-
-    handleResize = () => this.setState({ width: window.innerWidth });
-
-    render() {
-        console.log('render!');
-        return (
-            <MainWrapper>
-                <h2>EX5_useEffect_intermediate</h2>
-                <p>{`the width is: ${this.state.width}`}</p>
-                {this.state.warning && <p>It's too small!</p>}
-            </MainWrapper>
-        );
-    }
-}
+    return (
+        <MainWrapper>
+            <h2>EX5_useEffect_intermediate_solution</h2>
+            <p>{`the width is: ${width}`}</p>
+        </MainWrapper>
+    );
+};
 
 export default Main;
