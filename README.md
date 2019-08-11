440a71325ef904189ffab6e3bfc88824120b87bf
Stefano Le Pera <stefano.lepera@gmail.com>
2019-08-11 14:15:14 +0100

    Amended EX5

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 ## React Hooks Playground - EX5_useEffect_intermediate
 
-To solve this exercise, simply update the `Main.jsx` component using the `useEffect` to listen to the window resize.
-
-**Note: we use `PureComponent` here to optimize performance and avoid unnecessary re-renders**
-
+To solve this exercise, simply update the `Main.jsx` component using the `useEffect` to listen to the window resize and update the document title.<br>
 If you find yourself stuck or just want to double check your solution, please check out the [EX5_useEffect_intermediate_solution](https://github.com/stefanolepera/react-hooks-playground/tree/EX5_useEffect_intermediate_solution) branch.
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,42 +1,39 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { MainWrapper } from './Main.style';
 
-class Main extends PureComponent {
+class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            width: window.innerWidth,
-            isTooSmall: false
+            width: window.innerWidth
         };
     }
 
     componentDidMount() {
         window.addEventListener('resize', this.handleResize);
+        document.title = this.getTitle();
     }
 
     componentDidUpdate(_, prevState) {
         if (prevState.width !== this.state.width) {
-            if (this.state.width < 300) {
-                this.setState({ warning: true });
-            } else {
-                this.setState({ warning: false });
-            }
+            document.title = this.getTitle();
         }
     }
 
     componentWillUnmount() {
         window.removeEventListener('resize', this.handleResize);
+        document.title = '';
     }
 
     handleResize = () => this.setState({ width: window.innerWidth });
 
+    getTitle = () => (this.state.width < 300 ? "It's too small!" : "It's ok");
+
     render() {
-        console.log('render!');
         return (
             <MainWrapper>
                 <h2>EX5_useEffect_intermediate</h2>
                 <p>{`the width is: ${this.state.width}`}</p>
-                {this.state.warning && <p>It's too small!</p>}
             </MainWrapper>
         );
     }
