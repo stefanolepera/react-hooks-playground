3d83bd91e0446b8e55e854d595ab8921a5d1e009
berkayaskin <berkay.askin@bitechnology.com>
2020-04-26 06:38:16 +0300

    solved EX-1

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,29 +1,18 @@
-import React, { Component } from 'react';
-import { MainWrapper, Button } from './Main.style';
+import React, {useState} from 'react';
+import {MainWrapper, Button} from './Main.style';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            toggle: false
-        };
-    }
+function Main() {
+    const [toggle, setToggle] = useState(false);
 
-    switchToggle = () => {
-        this.setState({
-            toggle: !this.state.toggle
-        });
-    };
+    const handleToggle = () => setToggle(!toggle);
 
-    render() {
-        return (
-            <MainWrapper>
-                <h2>EX1_useState_basic</h2>
-                <p>{`the light is ${this.state.toggle ? 'ON' : 'OFF'}`}</p>
-                <Button onClick={this.switchToggle}>TOGGLE</Button>
-            </MainWrapper>
-        );
-    }
+    return (
+        <MainWrapper>
+            <h2>EX1_useState_basic</h2>
+            <p>{`the light is ${toggle ? 'ON' : 'OFF'}`}</p>
+            <Button onClick={handleToggle}>TOGGLE</Button>
+        </MainWrapper>
+    );
 }
 
 export default Main;
